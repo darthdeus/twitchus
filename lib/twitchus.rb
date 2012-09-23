899510e174ed195f430f418845c1d2a1cac7ef1d
Jakub Arnold <darthdeus@gmail.com>
2012-09-23 11:13:47 +0200

    Extract Twitchus::Config into a separate file

--- lib/twitchus.rb
+++ lib/twitchus.rb
@@ -1,16 +1,8 @@
 require "twitchus/version"
+require "twitchus/config"
 require "twitchus/cli"
 
 
 module Twitchus
 
-  class Config
-    attr_reader :streams
-
-    def load(file)
-      File.open(file) do |f|
-        @streams = f.readlines.map(&:strip)
-      end
-    end
-  end
 end
---
+++ lib/twitchus/config.rb
@@ -0,0 +1,11 @@
+module Twitchus
+  class Config
+    attr_reader :streams
+
+    def load(file)
+      File.open(file) do |f|
+        @streams = f.readlines.map(&:strip)
+      end
+    end
+  end
+end
