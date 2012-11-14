909baad59da9f84e5c50599e5fe7f3adc5509241
Jakub Arnold <darthdeus@gmail.com>
2012-11-14 16:51:35 +0100

    Fix specs not returning stringified JSON

--- spec/twitchus/checker_spec.rb
+++ spec/twitchus/checker_spec.rb
@@ -8,23 +8,23 @@ module Twitchus
 
       it "can tell if a channel is online" do
         stub_request(:get, "https://api.twitch.tv/kraken/streams/scvrush1")
-          .to_return(status: 200, body: { "stream" => {} })
+          .to_return(status: 200, body: { "stream" => {} }.to_json)
 
         checker.should be_online("scvrush1")
       end
 
       it "can tell if a channel is offline" do
         stub_request(:get, "https://api.twitch.tv/kraken/streams/scvrush1")
-          .to_return(status: 200, body: { "stream" => nil })
+          .to_return(status: 200, body: { "stream" => nil }.to_json)
 
         checker.should_not be_online("scvrush1")
       end
 
       it "selects online streams from a list" do
         stub_request(:get, "https://api.twitch.tv/kraken/streams/online")
-          .to_return(status: 200, body: { "stream" => { "name" => "online"} })
+          .to_return(status: 200, body: { "stream" => { "name" => "online"} }.to_json)
         stub_request(:get, "https://api.twitch.tv/kraken/streams/offline")
-          .to_return(status: 200, body: { "stream" => nil })
+          .to_return(status: 200, body: { "stream" => nil }.to_json)
 
         checker.fetch_all(%w(online offline)).should == [ { "name" => "online" } ]
       end
