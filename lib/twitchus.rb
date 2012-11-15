490212109be4e6361b37ca888e22aeff06f667a5
Jakub Arnold <darthdeus@gmail.com>
2012-11-15 12:31:35 +0100

    Add utf-8 encoding

--- lib/twitchus.rb
+++ lib/twitchus.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require_relative "twitchus/version"
 require_relative "twitchus/config"
 require_relative "twitchus/cli"
--- lib/twitchus/version.rb
+++ lib/twitchus/version.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module Twitchus
   VERSION = "0.1.1"
 end
